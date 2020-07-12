0600bebb2cc0b6fdc5c2de80a0f63b4c057f6d68
Brian Matt <bmatt@bmatt.net>
2020-07-12 00:31:01 -0400

    Fixing newlines

--- src/main/java/com/ecer/kafka/connect/oracle/OracleSourceConnectorUtils.java
+++ src/main/java/com/ecer/kafka/connect/oracle/OracleSourceConnectorUtils.java
@@ -460,6 +460,6 @@ public class OracleSourceConnectorUtils{
       if (str.startsWith("'") && str.endsWith("'"))str=str.substring(1,str.length()-1);        
       if (str.startsWith("\"") && str.endsWith("\"") && str.length()>1)str=str.substring(1,str.length()-1);
       return str.replace("IS NULL","= NULL").trim();
-    }       
+    }
 
 }
